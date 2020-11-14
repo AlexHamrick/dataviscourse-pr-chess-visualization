3e6ac2d1defb7bcb102a04fc7b5e3e58d494ac32
Stephen <stephen.topology@hotmail.com>
2020-11-13 23:09:04 -0700

    Chess openings table sorting implemented.

--- JS/chess_openings.js
+++ JS/chess_openings.js
@@ -32,10 +32,39 @@ class ChessOpenings {
             that.reducdedData.push(value);
         });
 
+        this.headerData = [
+            {
+                sorted: false,
+                ascending: false,
+                key: 'thOpenings'
+            },
+            {
+                sorted: false,
+                ascending: false,
+                key: 'thGames'
+            },
+            {
+                sorted: false,
+                ascending: false,
+                key: 'thWhite'
+            },
+            {
+                sorted: false,
+                ascending: false,
+                key: 'thDraw'
+            },
+            {
+                sorted: false,
+                ascending: false,
+                key: 'thBlack'
+            },
+        ]
+        this.attachSortHandlers();
         this.drawTable();
     }
 
     drawTable() {
+        d3.select("#openingsBody").selectAll("tr").remove();
         let rowSelection = d3.select("#openingsBody").selectAll("tr").data(this.reducdedData).join("tr");
         let tdSelection = rowSelection.selectAll("td").data(d => [d, d, d]).join("td");
         let openingCol = tdSelection.filter((d, i) => i === 0);
@@ -46,14 +75,14 @@ class ChessOpenings {
         let series = d3.stack().keys(["white", "draw", "black"])(this.reducdedData);
         let transpose = d3.transpose(series);
         let xScale = d3.scaleLinear().domain([0,1]).range([0,200]);
-        let svg = resultCol.append("svg").attr("width",300).attr("height", 50);
+        let svg = resultCol.append("svg").attr("width",300).attr("height", 40);
         let group = svg.append("g");
         let bars = group
             .selectAll("rect").data((d,i) => transpose[i]).join("rect")
             .attr("x",d => xScale(d[0]/d.data['games']))
             .attr("y", 10)
             .attr("width", d => xScale((d[1]-d[0])/d.data['games']))
-            .attr("height", 30)
+            .attr("height", 20)
             .attr("fill", function (d,i) {
                 if (i == 0) {
                     return 'white';
@@ -64,6 +93,95 @@ class ChessOpenings {
                 }
             })
             .attr("stroke", "black");
+    }
+
+    attachSortHandlers() {
+        let headers = d3.select("#openingsHeaders")
+            .selectAll(".sortable")
+            .data(this.headerData);
+        let that = this;
+        // e == event, d == thing you clicked
+        headers.on("click", function (e,d) {
+          let key = d.key;
+            let index = -1;
+            that.headerData.forEach(function(d,i) {
+                if (d.key == key) {
+                    index = i;
+                    d.sorted = true;
+                } else {
+                    d.sorted = false;
+                }
+            });
+            let hdr = that.headerData[index];
+            if (!hdr.ascending) {
+                that.reducdedData.sort(function(a,b) {
+                    return d3.ascending(a.eco, b.eco);
+                });
+            } else {
+                that.reducdedData.sort(function(a,b) {
+                    return d3.descending(a.eco, b.eco);
+                });
+            }
+            if (!hdr.ascending) {
+                switch (key) {
+                    case ('thOpenings'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.ascending(a.eco, b.eco);
+                        });
+                        break;
+                    case ('thGames'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.ascending(a.games, b.games);
+                        });
+                        break;
+                    case ('thWhite'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.ascending(a.white/a.games, b.white/b.games);
+                        });
+                        break;
+                    case ('thDraw'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.ascending(a.draw/a.games, b.draw/b.games);
+                        });
+                        break;
+                    case ('thBlack'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.ascending(a.black/a.games, b.black/b.games);
+                        });
+                        break;
+                }
+            } else {
+                switch (key) {
+                    case ('thOpenings'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.descending(a.eco, b.eco);
+                        });
+                        break;
+                    case ('thGames'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.descending(a.games, b.games);
+                        });
+                        break;
+                    case ('thWhite'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.descending(a.white/a.games, b.white/b.games);
+                        });
+                        break;
+                    case ('thDraw'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.descending(a.draw/a.games, b.draw/b.games);
+                        });
+                        break;
+                    case ('thBlack'):
+                        that.reducdedData.sort(function(a,b) {
+                            return d3.descending(a.black/a.games, b.black/b.games);
+                        });
+                        break;
+                }
+            }
+            hdr.ascending = !hdr.ascending;
+            that.drawTable();
+        });
     }
 
 }
