057b39216403a2562e3997f1a7c680a8d4b40e10
Cervantez <cervantezg@gmail.com>
2020-11-21 11:33:14 -0700

    Added drawHistogram function.

--- JS/gini_impurity.js
+++ JS/gini_impurity.js
@@ -4,14 +4,12 @@
 
 class GiniImpurity {
     constructor(data) {
-        this.data = data
-
         // Set up dimensions and margins
         this.margins = {
             top: 20,
-            right: 20,
+            right: 10,
             bottom: 20,
-            left: 20
+            left: 30
         }
 
         this.dimensions = {
@@ -19,18 +17,18 @@ class GiniImpurity {
             height: 350
         }
 
-        // Set up scales
+        // Set up x scale
         let minXRange = 0.5
         let maxXRange = 1
         this.xAxisScale = d3.scaleLinear()
             .domain([minXRange, maxXRange])
             .range([0, this.dimensions.width])
 
-        let minYRange = 15
-        let maxYRange = 0
-        this.densityYAxisScale = d3.scaleLinear()
-            .domain([minYRange, maxYRange])
-            .range([0, this.dimensions.height])
+        // let minYRange = 15
+        // let maxYRange = 0
+        // this.densityYAxisScale = d3.scaleLinear()
+        //     .domain([minYRange, maxYRange])
+        //     .range([0, this.dimensions.height])
 
         // let minYRange = 1200
         // let maxYRange = 0
@@ -38,41 +36,84 @@ class GiniImpurity {
         //     .domain([minYRange, maxYRange])
         //     .range([0, this.dimensions.height])
 
+        // Set up data
+        this.data = data
 
-        this.setUp()
+        let numberOfBins = 20
 
-        this.drawHistogram()
-    }
-
-    drawHistogram() {
         let histogram = d3.histogram()
             .value(d => d["GiniImpurity"])
             .domain(this.xAxisScale.domain())
-            .thresholds(this.xAxisScale.ticks(10))
+            .thresholds(this.xAxisScale.ticks(numberOfBins))
 
-        let bins = histogram(this.data)
+        this.bins = histogram(this.data)
 
+        // Adds the field selectedName to bins and sets it to false
+        this.setSelectedBin("")
+
+        // Do iniital drawing
+        this.setUp()
+        this.drawHistogram()
+    }
+
+    setSelectedBin(selectedName) {
         let foundName = false
-        let selectedName = "Blauert, Joerg"
 
-
-        for(let bin of bins) {
-            for(let binItem of bin) {
+        for (let bin of this.bins) {
+            for (let binItem of bin) {
                 if (binItem["WhiteName"] === selectedName) {
                     foundName = true
                     break
                 }
             }
 
-            if(foundName) {
+            if (foundName) {
                 bin["selectedBin"] = true
                 break
             } else {
                 bin["selectedBin"] = false
             }
         }
+    }
 
-        console.log(bins)
+    drawHistogram() {
+        let selectedName = "Donchenko, Anatoly G"
+        this.setSelectedBin(selectedName)
+
+        // Set up y axis
+        let histYAxisScale = d3.scaleLinear()
+            .range([this.dimensions.height, 0])
+            .domain([0, d3.max(this.bins, d => d["length"])])
+
+        console.log(histYAxisScale.domain())
+
+        // Get svg group for gini plot
+        let giniPlot = d3.select("#gini-plot")
+
+        // Create x-axis
+        giniPlot.append("g")
+            .attr("class", "gini-x-axis")
+            .attr("transform", "translate(0," + this.dimensions.height + ")")
+            .call(d3.axisBottom(this.xAxisScale))
+
+        // Create y-axis
+        giniPlot.append("g")
+            .attr("class", "gini-y-axis")
+            .call(d3.axisLeft(histYAxisScale))
+
+        // Draw histogram
+        giniPlot.selectAll("rect")
+            .data(this.bins)
+            .enter()
+            .append("rect")
+            .attr("x", 0)
+            .attr("transform", d => "translate(" + this.xAxisScale(d["x0"]) + "," + histYAxisScale(d["length"]) + ")")
+            .attr("width", d => this.xAxisScale(d["x1"]) - this.xAxisScale(d["x0"]))
+            .attr("height", d => this.dimensions.height - histYAxisScale(d["length"]))
+            .attr("class", d => d["selectedBin"] ? "selected-bin" : "")
+            .style("fill", d => d["selectedBin"] ? "red" : "gray")
+
+        console.log(this.bins)
     }
 
     setUp() {
@@ -86,54 +127,53 @@ class GiniImpurity {
 
         // Create a group to put plot in
         let giniPlot = svg.append("g")
-            .attr("class", "gini-plot")
+            .attr("id", "gini-plot")
             .attr("transform", "translate(" + this.margins.left + "," + this.margins.top + ")")
 
-        // Create x-axis
-        this.setUpAxes(giniPlot)
+
 
         // this.drawHistogram()
-        
 
 
-    //     ///////////////////
-    //     // set the parameters for the histogram
-    //     var histogram = d3.histogram()
-    //         .value(function(d) { return d['GiniImpurity']; })   // I need to give the vector of value
-    //         .domain(this.xAxisScale.domain())  // then the domain of the graphic
-    //         .thresholds(this.xAxisScale.ticks(10)); // then the numbers of bins
 
-    //       // And apply this function to data to get the bins
-    //     var bins = histogram(this.data);
+        //     ///////////////////
+        //     // set the parameters for the histogram
+        //     var histogram = d3.histogram()
+        //         .value(function(d) { return d['GiniImpurity']; })   // I need to give the vector of value
+        //         .domain(this.xAxisScale.domain())  // then the domain of the graphic
+        //         .thresholds(this.xAxisScale.ticks(10)); // then the numbers of bins
 
-    // //     let thresholds = this.xAxisScale.ticks(10)
+        //       // And apply this function to data to get the bins
+        //     var bins = histogram(this.data);
 
-    // //     let bins = d3.bin()
-    // //         .thresholds(thresholds)
-    // //         (this.data)
+        // //     let thresholds = this.xAxisScale.ticks(10)
 
-    //     console.log(bins)
+        // //     let bins = d3.bin()
+        // //         .thresholds(thresholds)
+        // //         (this.data)
 
-    //       // append the bar rectangles to the svg element
-    // svg.selectAll("rect")
-    // .data(bins)
-    // .enter()
-    // .append("rect")
-    //     .attr("x", 0)
-    //     .attr("transform", d => "translate(" + this.xAxisScale(d["x0"]) + "," + this.densityYAxisScale(d["length"]) + ")" )
-    //     .attr("width", d => this.xAxisScale(d.x1) - this.xAxisScale(d.x0) -1 )
-    //     .attr("height", d => this.dimensions.height - this.densityYAxisScale(d.length))
-    //     .style("fill", "#69b3a2")
+        //     console.log(bins)
 
-    //     svg.append("g")
-    //     .attr("fill", "#bbb")
-    //   .selectAll("rect")
-    //   .data(bins)
-    //   .join("rect")
-    //     .attr("x", d => this.xAxisScale(d.x0) ? 0 : this.xAxisScale(d.x0) + 1)
-    //     .attr("y", d => this.yAxisScale(d.length / this.data.length))
-    //     .attr("width", d => this.xAxisScale(d.x0) ? 0 : this.xAxisScale(d.x1) - this.xAxisScale(d.x0) - 1)
-    //     .attr("height", d => this.yAxisScale(0) - this.yAxisScale(d.length / this.data.length));
+        //       // append the bar rectangles to the svg element
+        // svg.selectAll("rect")
+        // .data(bins)
+        // .enter()
+        // .append("rect")
+        //     .attr("x", 0)
+        //     .attr("transform", d => "translate(" + this.xAxisScale(d["x0"]) + "," + this.densityYAxisScale(d["length"]) + ")" )
+        //     .attr("width", d => this.xAxisScale(d.x1) - this.xAxisScale(d.x0) -1 )
+        //     .attr("height", d => this.dimensions.height - this.densityYAxisScale(d.length))
+        //     .style("fill", "#69b3a2")
+
+        //     svg.append("g")
+        //     .attr("fill", "#bbb")
+        //   .selectAll("rect")
+        //   .data(bins)
+        //   .join("rect")
+        //     .attr("x", d => this.xAxisScale(d.x0) ? 0 : this.xAxisScale(d.x0) + 1)
+        //     .attr("y", d => this.yAxisScale(d.length / this.data.length))
+        //     .attr("width", d => this.xAxisScale(d.x0) ? 0 : this.xAxisScale(d.x1) - this.xAxisScale(d.x0) - 1)
+        //     .attr("height", d => this.yAxisScale(0) - this.yAxisScale(d.length / this.data.length));
 
         // svg.selectAll("rect")
         //     .data(bins)
@@ -147,38 +187,38 @@ class GiniImpurity {
 
         //////////////////////////
 
-    //     function kde(kernel, thresholds, data) {
-    //         return thresholds.map(t => [t, d3.mean(data, d => kernel(t - d))]);
-    //       }
+        //     function kde(kernel, thresholds, data) {
+        //         return thresholds.map(t => [t, d3.mean(data, d => kernel(t - d))]);
+        //       }
 
-    //       function epanechnikov(bandwidth) {
-    //         return x => Math.abs(x /= bandwidth) <= 1 ? 0.75 * (1 - x * x) / bandwidth : 0;
-    //       }
+        //       function epanechnikov(bandwidth) {
+        //         return x => Math.abs(x /= bandwidth) <= 1 ? 0.75 * (1 - x * x) / bandwidth : 0;
+        //       }
 
-    //     let bandwidth = 0.05
+        //     let bandwidth = 0.05
 
-    //     let line = d3.line()
-    //         .curve(d3.curveBasis)
-    //         .x(d => this.xAxisScale(d[0]))
-    //         .y(d => this.yAxisScale(d[1]))
+        //     let line = d3.line()
+        //         .curve(d3.curveBasis)
+        //         .x(d => this.xAxisScale(d[0]))
+        //         .y(d => this.yAxisScale(d[1]))
 
-    // let thresholds = this.xAxisScale.ticks(40)
+        // let thresholds = this.xAxisScale.ticks(40)
 
-    // let giniScores = this.data.map(d => d["GiniImpurity"])
+        // let giniScores = this.data.map(d => d["GiniImpurity"])
 
-    // let density = kde(epanechnikov(bandwidth), thresholds, giniScores)
+        // let density = kde(epanechnikov(bandwidth), thresholds, giniScores)
 
-    // console.log(density)
+        // console.log(density)
 
-    // svg.append("path")
-    //     .datum(density)
-    //     .attr("fill", "none")
-    //     .attr("stroke", "#000")
-    //     .attr("stroke-width", 1.5)
-    //     .attr("stroke-linejoin", "round")
-    //     .attr("d", line);
+        // svg.append("path")
+        //     .datum(density)
+        //     .attr("fill", "none")
+        //     .attr("stroke", "#000")
+        //     .attr("stroke-width", 1.5)
+        //     .attr("stroke-linejoin", "round")
+        //     .attr("d", line);
 
-  }      
+    }
 
     setUpAxes(giniPlot) {
         // Create x-axis
